a157b17144e16f5d91921d342e17d282c4dbf6e1
woongah <w00n9ah@gmail.com>
2024-01-15 12:04:26 +0900

    feat: Change int in range() 6 to 7

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ from random import choice
 
 
 def get_randint() -> list:
-    return [randint(1,45) for _ in range(6)]
+    return [randint(1,45) for _ in range(7)]
 
 
 def get_choice() -> list:
-    return [choice(range(1,45+1)) for _ in range(6)]
+    return [choice(range(1,45+1)) for _ in range(7)]
 
 if __name__=='__main__':
     print(get_randint())
