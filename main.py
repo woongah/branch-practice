2ba1998a3a2cf1ca14df7b650b5bd454ef665923
woongah <w00n9ah@gmail.com>
2024-01-15 11:47:51 +0900

    feat: Adjust Integer Range 1,45 to 1,50

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ from random import choice
 
 
 def get_randint() -> list:
-    return [randint(1,45) for _ in range(6)]
-
+	return [randint(1,50) for _ in range(6)]
 
 def get_choice() -> list:
-    return [choice(range(1,45+1)) for _ in range(6)]
+	return [choice(range(1,50+1)) for _ in range(6)]
+
 
 if __name__=='__main__':
-    print(get_randint())
-    print(get_choice())
+	print(get_randint())
+	print(get_choice())
+
